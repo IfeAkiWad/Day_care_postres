714b20402e093bb32f46f585f096fc22a2c05dbb
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-05 20:26:55 -0600

    namespacing routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :toddlers
-
-  resources :daycares, only: [:index, :show] do
+  namespace :api do
     resources :toddlers
+
+    resources :daycares, only: [:index, :show] do
+      resources :toddlers
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
